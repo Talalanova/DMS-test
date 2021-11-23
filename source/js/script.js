4f38e25ddf80d718d45f9cc02efd56c8e210922b
Talanova <pro_ego@mail.ru>
2021-11-23 18:32:02 +1000

    правки по пп, раскрывающийся список, стики хедер

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById('modal');
 const orderButton = document.querySelectorAll('.order-button');
-const wrap = document.getElementById('wrap');
+const overlay = document.querySelector('.modal__overlay')
+const equipmentButton = document.querySelectorAll('.equipment-list__button');
 
 const showModal = () => {
     modal.style.visibility = "visible";
@@ -8,14 +9,23 @@ const showModal = () => {
 
 const closeModal = () => {
     modal.style.visibility = "hidden";
+    modal.classList.remove('modal-active');
 }
 
 orderButton.forEach(button => {
     button.addEventListener('click', () => {
         showModal();
+        modal.classList.add('modal-active');
     })
 })
 
+overlay.addEventListener('click', () => {
+    closeModal();
+})
 
-
-
+equipmentButton.forEach(button => {
+    button.addEventListener('click', () => {
+        button.parentElement.parentElement.classList.toggle('equipment-item--open');        
+        button.lastChild.classList.toggle('rotate-svg');
+    })
+})
